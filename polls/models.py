f0cd81e4bffcdf5ff60436ce6e2700f39b83e2b2
mcueto <cueto@live.cl>
2019-02-22 02:56:53 -0300

    Create Poll model in polls app
    - Create model in polls/models.py
    - Generate migration for Poll model

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,49 @@
+"""polls app models."""
+import uuid
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+# Fields constants
+CODE_FIELD_MAX_LENGTH = 128
+NAME_FIELD_MAX_LENGTH = 128
+
+# Poll status field choices
+POLL_STATUS_CHOICES = (
+    ('created', 'created'),
+    ('open', 'open'),
+    ('finished', 'finished'),
+    ('archived', 'archived'),
+)
+
+
+class Poll(models.Model):
+    unique_id = models.UUIDField(
+        default=uuid.uuid4,
+        editable=False
+    )
+    code = models.CharField(
+        max_length=CODE_FIELD_MAX_LENGTH
+    )
+    title = models.CharField(
+        max_length=NAME_FIELD_MAX_LENGTH
+    )
+    description = models.TextField(
+        blank=True
+    )
+    current_status = models.CharField(
+        max_length=CODE_FIELD_MAX_LENGTH,
+        choices=POLL_STATUS_CHOICES
+    )
+    created_at = models.DateTimeField(
+        default=timezone.now
+    )
+    updated_at = models.DateTimeField(
+        default=timezone.now
+    )
+
+    def __str__(self):
+        """Return the model instance item name in django admin."""
+        return '{title} - {created_at}'.format(
+            title=self.title,
+            created_at=self.created_at
+        )
