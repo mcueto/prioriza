00019a8a12eea69142b6023eeae9d3ca0686805a
mcueto <cueto@live.cl>
2019-02-22 01:47:45 -0300

    Set SECRET_KEY and DEBUG settings as environment variables
    - Settings in prioriza/settings.py set as env var
    - Development env vars added to setenvvars.sh file

--- prioriza/settings.py
+++ prioriza/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '67#%)yp!idi_+)n6iw^5ud&29=q%a@l)l#3@u*tkehrk&y6mi+'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = eval(os.environ.get('DEBUG', 'True'))
 
 ALLOWED_HOSTS = []
 
---
+++ setenvvars.sh
@@ -0,0 +1,2 @@
+export SECRET_KEY='67#%)yp!idi_+)n6iw^5ud&29=q%a@l)l#3@u*tkehrk&y6mi+'
+export DEBUG=True
