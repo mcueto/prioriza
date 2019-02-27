dd6f67351efc31fbf8b4860bb24e468f54756f6d
mcueto <cueto@live.cl>
2019-02-27 01:50:07 -0300

    Create viewsets for PollOption & PollOptionSelection models

--- polls/viewsets.py
+++ polls/viewsets.py
@@ -3,12 +3,26 @@ from rest_framework import (
 )
 from .models import (
     Poll,
+    PollOption,
+    PollOptionSelection,
 )
 from .serializers import (
     PollSerializer,
+    PollOptionSerializer,
+    PollOptionSelectionSerializer,
 )
 
 
 class PollViewSet(viewsets.ModelViewSet):
     serializer_class = PollSerializer
     queryset = Poll.objects.all()
+
+
+class PollOptionViewSet(viewsets.ModelViewSet):
+    serializer_class = PollOptionSerializer
+    queryset = PollOption.objects.all()
+
+
+class PollOptionSelectionViewSet(viewsets.ModelViewSet):
+    serializer_class = PollOptionSelectionSerializer
+    queryset = PollOptionSelection.objects.all()
