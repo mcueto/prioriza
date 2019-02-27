926e69fb1704fa346778aa3d5d9a06b8250e8c8d
mcueto <cueto@live.cl>
2019-02-27 01:39:47 -0300

    Create PollOptionSerializer and PollOptionSelectionSerializer

--- polls/serializers.py
+++ polls/serializers.py
@@ -3,12 +3,30 @@ from rest_framework import (
 )
 from .models import (
     Poll,
+    PollOption,
+    PollOptionSelection,
 )
 
 
 class PollSerializer(serializers.ModelSerializer):
     class Meta:
         model = Poll
+        fields = (
+            '__all__'
+        )
+
+
+class PollOptionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PollOption
+        fields = (
+            '__all__'
+        )
+
+
+class PollOptionSelectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PollOptionSelection
         fields = (
             '__all__'
         )
