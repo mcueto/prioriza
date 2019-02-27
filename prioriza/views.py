5bd32f29baae2429580fda89088dc74888e18146
mcueto <cueto@live.cl>
2019-02-27 06:41:22 -0300

    - Remove message context variable - Add link to login user

--- prioriza/views.py
+++ prioriza/views.py
@@ -13,12 +13,10 @@ from polls.models import (
 
 
 class IndexView(TemplateView):
-
     template_name = "index.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['message'] = 'PRIORIZA INDEX VIEW TEMPLATE'
 
         return context
 
