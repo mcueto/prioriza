f2d33cdcd29ee3dad9255008ac3494b98b67569d
mcueto <cueto@live.cl>
2019-02-28 02:43:16 -0300

    Add PollVote model to django admin site

--- polls/admin.py
+++ polls/admin.py
@@ -3,8 +3,10 @@ from .models import (
     Poll,
     PollOption,
     PollOptionSelection,
+    PollVote,
 )
 
 admin.site.register(Poll)
 admin.site.register(PollOption)
 admin.site.register(PollOptionSelection)
+admin.site.register(PollVote)
