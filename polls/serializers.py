54b0648cffce6d6cd06d67dd4d79743bf849c5ac
mcueto <cueto@live.cl>
2019-02-28 03:05:39 -0300

    Create PollOptionSelectionCreateSerializer & PollVoteCreateSerializer

--- polls/serializers.py
+++ polls/serializers.py
@@ -62,6 +62,17 @@ class PollOptionCreateSerializer(serializers.ModelSerializer):
         )
 
 
+class PollOptionSelectionCreateSerializer(serializers.Serializer):
+    poll_option_unique_id = serializers.UUIDField()
+    priority = serializers.IntegerField()
+
+    class Meta:
+        fields = (
+            'poll_option_unique_id',
+            'priority',
+        )
+
+
 class PollCreateSerializer(serializers.Serializer):
     poll = PollCreateSerializer()
     poll_options = PollOptionCreateSerializer(many=True)
@@ -70,4 +81,15 @@ class PollCreateSerializer(serializers.Serializer):
         fields = (
             'poll',
             'poll_options',
+        )
+
+
+class PollVoteCreateSerializer(serializers.Serializer):
+    poll_unique_id = serializers.UUIDField()
+    poll_option_selections = PollOptionSelectionCreateSerializer(many=True)
+
+    class Meta:
+        fields = (
+            'poll_unique_id',
+            'poll_option_selections',
         )
